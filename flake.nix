190a5bd46042c642144f8b7f709e4f8f732af4f7
Rob Harrop <robharrop@vetinari.local>
2023-06-28 20:52:57 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Systemm configuration flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    mkAlias = {
+      url = "github:reckenrode/mkAlias";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  # add the inputs declared above to the argument attribute set
+  outputs = inputs @ { self, nixpkgs, home-manager, darwin, flake-utils, ... }:
+
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-linux" ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          formatter = pkgs.nixpkgs-fmt;
+        }
+      ) // {
+
+      darwinConfigurations."vetinari" =
+        let
+          arch = "aarch64-darwin";
+        in
+        darwin.lib.darwinSystem {
+
+          inputs = inputs // { inherit arch; };
+
+          system = arch;
+          modules = [ 
+            home-manager.darwinModules.home-manager
+            ./hosts/vetinari/default.nix 
+          ]; 
+        };
+    };
+
+
+}
---
+++ hosts/vetinari/default.nix
@@ -0,0 +1,125 @@
+{ config, pkgs, lib, inputs, home-manager, ... }:
+
+let
+  username = "robharrop";
+
+in
+{
+
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    with pkgs;[
+      emacs
+      git
+      mas
+      vim
+      obsidian
+      raycast
+      )
+    ];
+
+  fonts.fontDir.enable = true;
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  programs.zsh.enable = true;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  system.defaults = {
+    dock.autohide = true;
+    # use standard function keys
+    NSGlobalDomain."com.apple.keyboard.fnState" = true;
+  };
+
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+  };
+
+
+  homebrew = {
+    enable = true;
+    
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "uninstall";
+    };
+    
+    casks = [
+      "firefox"
+      "1password"
+      "whatsapp"
+      "chromium"
+    ];
+
+    masApps = {
+      "Todoist: To-Do List & Tasks" = 585829637;
+    };
+  };
+
+home-manager.useGlobalPkgs = true;
+home-manager.useUserPackages = false;
+home-manager.users.robharrop = { pkgs, ... }: {
+
+  home.stateVersion = "23.05"; 
+
+  programs.home-manager.enable = true;
+
+  programs.exa = {
+    enable = true;
+    enableAliases = true;  
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.kitty = {
+    enable = true;
+    environment = {
+      "LS_COLORS" = "1";
+    };
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 14;
+    };
+    settings = {
+      macos_option_as_alt = "yes";
+      macos_titlebar_color = "#282a36";
+    };
+    theme = "Dracula";
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
+  programs.vscode = {
+    enable = true;
+    extensions = with vscode-extensions; [
+      bbenoist.nix
+    ];
+  }
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+  };
+};
+
+users.users.robharrop = {
+  name = "robharrop";
+  home = "/Users/robharrop";
+};
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
